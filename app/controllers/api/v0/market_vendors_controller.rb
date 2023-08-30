8cefb86eadcdb840506c2a5ee0ee1f8fb07deef2
Derek Chavez <frrenose17@yahoo.com>
2023-08-30 10:56:48 -0600

    feat: add create method in market vendors controller

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,5 +1,20 @@
 class Api::V0::MarketVendorsController < ApplicationController
   def create
+    vendor = Vendor.find_by(id: market_vendor_params[:vendor_id])
 
+    if vendor.nil?
+      render json: { errors: "Vendor not found" }, status: :not_found
+    else
+      market_vendor = MarketVendor.create!(market_vendor_params)
+      render json: MarketVendorSerializer.new(market_vendor), status: 201
+    end
+  rescue ActiveRecord::RecordInvalid => error
+    render json: { errors: error.record.errors.full_messages.join(', ') }, status: :bad_request
+  end
+
+  private
+
+  def market_vendor_params
+    params.require(:market_vendor).permit(:market_id, :vendor_id)
   end
 end
--- spec/requests/api/v0/market_vendors/create_request_spec.rb
+++ spec/requests/api/v0/market_vendors/create_request_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe "MarketVendors API" do
       post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
 
       expect(response).to be_successful
+      expect(response).to have_http_status(201)
+
+      attributes = JSON.parse(response.body)["data"]["attributes"]
+
+      expect(attributes["market_id"]).to eq(market_1.id)
+      expect(attributes["vendor_id"]).to eq(vendor_1.id)
+    end
+
+    it "returns a 404 status code and a error message if an invalid vendor id is passed in" do
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+      invalid_id = 923846039
+      market_vendor_params = { market_id: market_1.id,
+                               vendor_id: invalid_id }
+
+      headers = { "CONTENT_TYPE": "application/json" }
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(404)
     end
   end
 end
