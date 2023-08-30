652088e3822dcf8c64135b822c8cce6cde2d1c2b
Derek Chavez <frrenose17@yahoo.com>
2023-08-30 11:43:19 -0600

    feat: add sad path for existing assocation/market_vendors

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,12 +1,26 @@
 class Api::V0::MarketVendorsController < ApplicationController
   def create
+    if market_vendor_params[:market_id].nil? || market_vendor_params[:vendor_id].nil?
+      render json: { errors: "Both market ID and vendor ID are required" }, status: :bad_request
+      return
+    end
+
+    existing_association = MarketVendor.find_by(market_id: market_vendor_params[:market_id], vendor_id: market_vendor_params[:vendor_id])
+    if existing_association
+      render json: { errors: "Association already exists" }, status: :unprocessable_entity
+      return
+    end
+
+    market = Market.find_by(id: market_vendor_params[:market_id])
     vendor = Vendor.find_by(id: market_vendor_params[:vendor_id])
 
-    if vendor.nil?
+    if market.nil?
+      render json: { errors: "Market not found" }, status: :not_found
+    elsif vendor.nil?
       render json: { errors: "Vendor not found" }, status: :not_found
     else
       market_vendor = MarketVendor.create!(market_vendor_params)
-      render json: MarketVendorSerializer.new(market_vendor), status: 201
+      render json: MarketVendorSerializer.new(market_vendor), status: :created
     end
   rescue ActiveRecord::RecordInvalid => error
     render json: { errors: error.record.errors.full_messages.join(', ') }, status: :bad_request
--- spec/requests/api/v0/market_vendors/create_request_spec.rb
+++ spec/requests/api/v0/market_vendors/create_request_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "MarketVendors API" do
       expect(attributes["vendor_id"]).to eq(vendor_1.id)
     end
 
-    it "returns a 404 status code and a error message if an invalid vendor id is passed in" do
+    it "returns a 404 status code and an error message if an invalid vendor id is passed in" do
       market_1 = create(:market)
       vendor_1 = create(:vendor)
       invalid_id = 923846039
@@ -31,9 +31,61 @@ RSpec.describe "MarketVendors API" do
       headers = { "CONTENT_TYPE": "application/json" }
 
       post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
-
+      
       expect(response).to_not be_successful
       expect(response).to have_http_status(404)
+      error_response = JSON.parse(response.body)
+      expect(error_response["errors"]).to eq("Vendor not found")
+    end
+    
+    it "returns a 404 status code and an error message if an invalid market id is passed" do
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+      invalid_id = 923846039
+      market_vendor_params = { market_id: invalid_id,
+                               vendor_id: vendor_1.id }
+      
+      headers = { "CONTENT_TYPE": "application/json" }
+      
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+      
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(404)
+      error_response = JSON.parse(response.body)
+      expect(error_response["errors"]).to eq("Market not found")
+    end
+    
+    it "returns a 400 status code and an error message if both market/vendor id are missing" do
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+      market_vendor_params = { market_id: nil,
+      vendor_id: nil }
+      
+      headers = { "CONTENT_TYPE": "application/json" }                       
+      
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+      
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(400)
+      error_response = JSON.parse(response.body)
+      expect(error_response["errors"]).to eq("Both market ID and vendor ID are required")
+    end
+    
+    it "returns a 422 status code and an error message if the association between market and vendor already exists" do
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+      create(:market_vendor, market: market_1, vendor: vendor_1)
+      market_vendor_params = { market_id: market_1.id, 
+      vendor_id: vendor_1.id }
+      
+      headers = { "CONTENT_TYPE": "application/json" }
+      
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(422)
+      error_response = JSON.parse(response.body)
+      expect(error_response["errors"]).to eq("Association already exists")
     end
   end
 end
